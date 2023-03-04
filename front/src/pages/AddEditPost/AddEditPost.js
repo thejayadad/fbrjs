0018a27a3a50d8ee93f5024b520774253d27bbb2
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-04 07:49:09 -0500

    update

--- front/src/pages/AddEditPost/AddEditPost.js
+++ front/src/pages/AddEditPost/AddEditPost.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useNavigate, useParams } from "react-router-dom";
 
+
+
+const initialState = {
+    title: "",
+    tags: [],
+    trending: "no",
+    category: "",
+    description: "",
+    comments: [],
+    likes: []
+  };
+
+  const categoryOption = [
+    "Fashion",
+    "Technology",
+    "Food",
+    "Politics",
+    "Sports",
+    "Business",
+  ];
 const AddEditPost = () => {
+    const [form, setForm] = useState(initialState);
+  const [file, setFile] = useState(null);
+
+
+  const { title, tags, category, trending, description } = form;
+  const { id } = useParams();
+
   return (
-    <div>AddEditPost</div>
+    <div>
+
+        { id ? "Update Post" : "Create Post"}
+        <form>
+        <input
+                  type="text"
+                  className="form-control input-text-box"
+                  placeholder="Title"
+                  name="title"
+                  value={title}
+                  onChange={handleChange}
+                />
+                <ReactTagInput />
+        </form>
+    </div>
   )
 }
 
